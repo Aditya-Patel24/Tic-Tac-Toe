5650dc4ca9445c981f599d2cbe5ca62ba5a8305d
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-01 22:23:34 +0530

    Add build configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/Tic-Tac-Toe/',
+  build: {
+    outDir: 'build', // This should match the directory you're trying to deploy
+  },
   plugins: [react()],
 })
